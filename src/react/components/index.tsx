ee8afbece53e37793a251d604928eb4ecc46755f
dongntd <dongntd267@gmail.com>
2024-03-28 19:45:13 +0700

    dongntd: init todo-react

--- src/react/components/index.tsx
+++ src/react/components/index.tsx
@@ -4,9 +4,19 @@
  *
  */
 
+import Header from '@components/Header';
+import Main from '@components/Main';
+import Footer from '@components/Footer';
+
 /** global styles */
 import './global.css';
 
 export default function App() {
-    return <div>App</div>;
+    return (
+        <>
+            <Header />
+            <Main />
+            <Footer />
+        </>
+    );
 }
